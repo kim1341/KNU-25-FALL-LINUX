dcce9ee5320cdbaae869717fa8aa6c1ba0041a23
Dasom Kim <dskim134@gmail.com>
2025-12-13 23:05:33 +0900

    update

--- KNU-25-FALL-LINUX/rinux.c
+++ KNU-25-FALL-LINUX/rinux.c
@@ -4,212 +4,202 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <time.h>
-#include <string.h>
+#include <stdbool.h>
 
 #define N 10
-#define TQ 10
+#define DEFAULT_TQ 10
 
-#define READY 0
-#define SLEEP 1
-#define DONE 2
+typedef enum {
+    STATE_READY,
+    STATE_RUNNING,
+    STATE_SLEEP,
+    STATE_DONE
+} ProcessState;
 
 typedef struct {
-    int pid;
+    pid_t pid;
     int id;
     int remaining_tq;
-    int state;
-    int io_timer;
-    int remaining_burst;
-    
-    int fd_to_child[2];
-    int fd_from_child[2];
+    ProcessState state;
+    int io_wait_time;
+    int fd_p2c[2];
+    int fd_c2p[2];
     
     int arrival_time;
     int finish_time;
     int waiting_time;
 } PCB;
 
-PCB proc[N];
+PCB pcb_table[N];
 
-void do_child_work(int id) {
-    srand(time(NULL) ^ getpid()); 
-
-    int cpu_burst = (rand() % 10) + 1; 
-    
+void child_process_logic(int id, int read_fd, int write_fd) {
+    srand(time(NULL) ^ getpid());
+    int cpu_burst = (rand() % 10) + 1;
     int cmd;
-    int report[2];
+    int response; 
 
     while (1) {
-        read(proc[id].fd_to_child[0], &cmd, sizeof(int));
+        read(read_fd, &cmd, sizeof(int));
 
         cpu_burst--;
 
         if (cpu_burst <= 0) {
-            report[0] = 3;
-        } 
-        else {
-            if (rand() % 10 < 3) { 
-                report[0] = 2;
+            if (rand() % 2 == 0) {
+                response = 2; 
+                write(write_fd, &response, sizeof(int));
+                cpu_burst = (rand() % 10) + 1;
             } else {
-                report[0] = 1;
+                response = 3; 
+                write(write_fd, &response, sizeof(int));
+                exit(0);
             }
+        } else {
+            response = 1; 
+            write(write_fd, &response, sizeof(int));
         }
-        
-        report[1] = cpu_burst; 
-        
-        write(proc[id].fd_from_child[1], report, sizeof(report));
-
-        if (report[0] == 3) exit(0);
     }
 }
 
 int main() {
     srand(time(NULL));
 
-    printf("=== OS 시뮬레이션 시작 (TQ: %d) ===\n", TQ);
-
     for (int i = 0; i < N; i++) {
-        pipe(proc[i].fd_to_child);
-        pipe(proc[i].fd_from_child);
+        if (pipe(pcb_table[i].fd_p2c) == -1 || pipe(pcb_table[i].fd_c2p) == -1) {
+            perror("파이프 생성 실패");
+            exit(1);
+        }
 
-        int pid = fork();
+        pid_t pid = fork();
 
         if (pid == 0) {
-            close(proc[i].fd_to_child[1]);
-            close(proc[i].fd_from_child[0]);
-            do_child_work(i);
-        } 
-        else {
-            proc[i].pid = pid;
-            proc[i].id = i;
-            proc[i].remaining_tq = TQ;
-            proc[i].state = READY;
-            proc[i].io_timer = 0;
-            proc[i].remaining_burst = -1;
-            
-            proc[i].arrival_time = 0;
-            proc[i].waiting_time = 0;
-            proc[i].finish_time = 0;
+            close(pcb_table[i].fd_p2c[1]);
+            close(pcb_table[i].fd_c2p[0]);
+            child_process_logic(i, pcb_table[i].fd_p2c[0], pcb_table[i].fd_c2p[1]);
+            exit(0);
+        } else {
+            close(pcb_table[i].fd_p2c[0]);
+            close(pcb_table[i].fd_c2p[1]);
 
-            close(proc[i].fd_to_child[0]);
-            close(proc[i].fd_from_child[1]);
+            pcb_table[i].pid = pid;
+            pcb_table[i].id = i;
+            pcb_table[i].remaining_tq = DEFAULT_TQ;
+            pcb_table[i].state = STATE_READY;
+            pcb_table[i].io_wait_time = 0;
+            
+            pcb_table[i].arrival_time = 0;
+            pcb_table[i].finish_time = 0;
+            pcb_table[i].waiting_time = 0;
         }
     }
 
-    int active_count = N;
+    int active_processes = N;
     int current_idx = 0;
     int global_time = 0;
 
-    while (active_count > 0) {
-        sleep(1);
+    while (active_processes > 0) {
+        usleep(100000); 
         global_time++;
+        printf("\n[시간 %d]\n", global_time);
+
+        for (int i = 0; i < N; i++) {
+            if (pcb_table[i].state == STATE_SLEEP) {
+                pcb_table[i].io_wait_time--;
+                if (pcb_table[i].io_wait_time <= 0) {
+                    pcb_table[i].state = STATE_READY;
+                    printf("프로세스 %d 대기 완료 (Ready 상태로 복귀)\n", i);
+                }
+            }
+            else if (pcb_table[i].state == STATE_READY) {
+                pcb_table[i].waiting_time++;
+            }
+        }
+
+        bool all_tq_zero = true;
+        for (int i = 0; i < N; i++) {
+            if (pcb_table[i].state != STATE_DONE && pcb_table[i].remaining_tq > 0) {
+                all_tq_zero = false;
+                break;
+            }
+        }
+
+        if (all_tq_zero) {
+            printf("모든 프로세스 타임 퀀텀 소진. 전체 프로세스 TQ 초기화.\n");
+            for (int i = 0; i < N; i++) {
+                if (pcb_table[i].state != STATE_DONE) {
+                    pcb_table[i].remaining_tq = DEFAULT_TQ;
+                }
+            }
+        }
+
+        int target_idx = -1;
+        int start_idx = current_idx;
         
-        for(int i=0; i<N; i++) {
-            if(proc[i].state == SLEEP) {
-                proc[i].io_timer--;
-                if(proc[i].io_timer <= 0) proc[i].state = READY;
-            }
-            else if(proc[i].state == READY) {
-                proc[i].waiting_time++;
-            }
-        }
-
-        printf("[시간 %2d] Ready 큐: [ ", global_time);
-        int print_cnt = 0;
-        for(int i=0; i<N; i++) {
-            int idx = (current_idx + i) % N;
-            if (proc[idx].state == READY && proc[idx].remaining_tq > 0) {
-                printf("%d ", proc[idx].id);
-                print_cnt++;
-            }
-        }
-        if(print_cnt == 0) printf("비어있음 ");
-        printf("] ");
-
-        int target = -1;
-        for(int i=0; i<N; i++) {
-            int idx = (current_idx + i) % N;
-            if(proc[idx].state == READY && proc[idx].remaining_tq > 0) {
-                target = idx;
+        for (int i = 0; i < N; i++) {
+            int idx = (start_idx + i) % N;
+            if (pcb_table[idx].state == STATE_READY && pcb_table[idx].remaining_tq > 0) {
+                target_idx = idx;
                 current_idx = idx;
                 break;
             }
         }
 
-        if (target == -1) {
-            int still_alive = 0;
-            for(int i=0; i<N; i++) {
-                if(proc[i].state != DONE) {
-                    proc[i].remaining_tq = TQ;
-                    still_alive++;
-                }
-            }
-            if(still_alive == 0) {
-                printf("\n");
-                break;
-            }
-            printf("-> (IDLE) 실행 대기 중...\n");
+        if (target_idx == -1) {
+            printf("IDLE (대기 중인 프로세스 없음)\n");
             continue;
         }
 
+        if (pcb_table[target_idx].waiting_time > 0) {
+            pcb_table[target_idx].waiting_time--;
+        }
+
         int cmd = 1;
-        write(proc[target].fd_to_child[1], &cmd, sizeof(int));
-        proc[target].remaining_tq--;
+        write(pcb_table[target_idx].fd_p2c[1], &cmd, sizeof(int));
         
-        int report[2];
-        read(proc[target].fd_from_child[0], report, sizeof(report));
+        pcb_table[target_idx].remaining_tq--;
         
-        int status = report[0];
-        proc[target].remaining_burst = report[1];
+        int response;
+        read(pcb_table[target_idx].fd_c2p[0], &response, sizeof(int));
 
-        if (status == 3) {
-            printf("-> P%d 종료 (남은Burst: 0)\n", target);
-            proc[target].state = DONE;
-            proc[target].finish_time = global_time;
-            
-            if (proc[target].waiting_time > 0) proc[target].waiting_time--; 
-            
-            active_count--;
+        if (response == 3) {
+            printf("프로세스 %d 종료됨\n", target_idx);
+            pcb_table[target_idx].state = STATE_DONE;
+            pcb_table[target_idx].finish_time = global_time;
+            active_processes--;
+            waitpid(pcb_table[target_idx].pid, NULL, 0);
+        } else if (response == 2) {
+            int wait_time = (rand() % 5) + 1;
+            pcb_table[target_idx].state = STATE_SLEEP;
+            pcb_table[target_idx].io_wait_time = wait_time;
+            printf("프로세스 %d I/O 요청 (대기시간: %d, 남은 TQ: %d)\n", target_idx, wait_time, pcb_table[target_idx].remaining_tq);
+        } else {
+            printf("프로세스 %d 실행 중 (남은 TQ: %d)\n", target_idx, pcb_table[target_idx].remaining_tq);
         }
-        else if (status == 2) {
-            printf("-> P%d I/O 요청 (남은Burst:%d, 남은TQ:%d)\n", 
-                   target, proc[target].remaining_burst, proc[target].remaining_tq);
-            proc[target].state = SLEEP;
-            proc[target].io_timer = (rand() % 4) + 1;
-            
-            if (proc[target].waiting_time > 0) proc[target].waiting_time--;
-        }
-        else {
-            printf("-> P%d 실행 (남은Burst:%d, 남은TQ:%d)\n", 
-                   target, proc[target].remaining_burst, proc[target].remaining_tq);
-            
-            if (proc[target].waiting_time > 0) proc[target].waiting_time--;
 
-            if(proc[target].remaining_tq == 0) {
-                printf("          (P%d TQ 만료! 순서 변경)\n", target);
-                current_idx = (current_idx + 1) % N;
-            }
+        if (pcb_table[target_idx].state == STATE_READY && pcb_table[target_idx].remaining_tq == 0) {
+             printf("프로세스 %d 타임 퀀텀 만료\n", target_idx);
+             current_idx = (target_idx + 1) % N;
         }
     }
 
-    for(int i=0; i<N; i++) wait(NULL);
-
     printf("\n\n");
     printf("=========================================================\n");
-    printf("                 성능 분석표 (TQ = %d)\n", TQ);
+    printf("                  성능 분석표 (TQ = %d)\n", DEFAULT_TQ);
     printf("=========================================================\n");
-    printf(" ID (Real PID) | 종료시간 | 대기시간(WT) | 반환시간(TT)\n");
-    printf("---------------+----------+--------------+---------------\n");
+    printf(" PID | 종료시간 | 대기시간(WT) | 반환시간(TT)\n");
+    printf("-----+----------+--------------+---------------\n");
 
     double total_wait = 0;
     double total_turnaround = 0;
 
-    for(int i=0; i<N; i++) {
-        int turnaround = proc[i].finish_time - proc[i].arrival_time;
-        printf("  %d (%d)  |    %3d   |      %3d     |      %3d\n", 
-            proc[i].id, proc[i].pid, 
-            proc[i].finish_time, proc[i].waiting_time, turnaround);
-        total_wait += proc[i].waiting_time;
+    for (int i = 0; i < N; i++) {
+        int turnaround = pcb_table[i].finish_time - pcb_table[i].arrival_time;
+        printf(" %3d |    %3d   |      %3d     |      %3d\n", 
+            pcb_table[i].pid, 
+            pcb_table[i].finish_time, 
+            pcb_table[i].waiting_time, 
+            turnaround);
+        
+        total_wait += pcb_table[i].waiting_time;
         total_turnaround += turnaround;
     }
 
